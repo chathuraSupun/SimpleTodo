33abdd86a16cb8df6dfb2e2177a52c3533082d74
iamSupunChathura <supunchathura920323@gmail.com>
2021-08-24 10:43:20 +0530

    storybook removed from app.tsx

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
-// import React from "react";
-// import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-// export default function App() {
-//     return (
-//         <View style={styles.container}>
-//             <Text>Welcome Open up App.tsx to start working on your app!</Text>
-//         </View>
-//     );
-// }
+export default function App() {
+    return (
+        <View style={styles.container}>
+            <Text>Welcome Open up App.tsx to start working on your app!</Text>
+        </View>
+    );
+}
 
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         backgroundColor: "#fff",
-//         alignItems: "center",
-//         justifyContent: "center"
-//     }
-// });
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center"
+    }
+});
 
-export { default } from "./storybook";
+// export { default } from "./storybook";
